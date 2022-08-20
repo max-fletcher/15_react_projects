71b3665e6f6e47582fc5110120a041290418f618
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2022-08-20 23:59:49 +0600

    Started Tours.

--- 2-tours/src/App.js
+++ 2-tours/src/App.js
@@ -5,7 +5,18 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-   return <h2>Tours Project Setup</h2>
+
+   const [loading, setLoading] = useState(true)
+   const [tours, setTours] = useState([])
+
+   if(loading){
+      <main>
+         <Loading />
+      </main>
+   }
+   else{
+      return <h2>Tours Project Setup</h2>
+   }
 }
 
 export default App
