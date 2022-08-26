afdf9d46545679998d1b26911b4cf0d8df313e92
Mahin Chowdhury <amar.naam.machine@gmail.com>
2022-08-26 21:11:18 +0600

    6-tabs completed.

--- 6-tabs/src/App.js
+++ 6-tabs/src/App.js
@@ -37,10 +37,31 @@ function App() {
         <div className='title'>
           <h2> Experience </h2>
           <div className='underline'></div>
-          <div className='jobs-center'> 
-            {/* button container */}
-            {/* job info */}
+        </div>
+        <div className='jobs-center'>
+          {/* button container */}
+          <div className="btn-container">
+            {
+              jobs.map((job, index) => {
+                return <button key={job.id} onClick={() => {setValue(index)}}
+                  className={`job-btn ${index === value && 'active-btn'}`}>{job.company}</button>
+              })
+            }
           </div>
+          {/* job info */}
+          <article className='job-info'>
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {
+              duties.map((duty, index) => {
+                return <div key={index} className='job-desc'>
+                  <FaAngleDoubleRight className='job-icon' />
+                  <p>{duty}</p>
+                </div>
+              })
+            }
+          </article>
         </div>
       </section>
     )
